c7229b1dc27d4f5f74901031257fa5d28db54f2b
Ecodes-lab <myecodevelopment@gmail.com>
2020-02-11 23:05:51 -0800

    Updates

---
+++ precache-manifest.42d6284176d47bf34696923874a0ec4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f741bc9109e48b362caf967294203db6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "50b4c4a4e5b0e8aedec3",
+    "url": "/portfolio/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b52d1258e06e2c0dda9b",
+    "url": "/portfolio/static/js/2.40710d27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.40710d27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50b4c4a4e5b0e8aedec3",
+    "url": "/portfolio/static/js/main.8e519d12.chunk.js"
+  },
+  {
+    "revision": "debb28456858994a1826",
+    "url": "/portfolio/static/js/runtime-main.3e206433.js"
+  }
+]);
